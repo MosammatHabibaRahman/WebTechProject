f309f5beacbb0ed377ade39f4e81b3616ff2e8b1
MosammatHabibaRahman <60427978+MosammatHabibaRahman@users.noreply.github.com>
2020-05-11 13:07:23 +0600

    Dummy DB

---
+++ webtech_project_db.sql
@@ -0,0 +1,194 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 16, 2020 at 07:44 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webtech_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `c_id` int(5) NOT NULL,
+  `course_name` varchar(50) NOT NULL,
+  `no_of_classes` int(5) NOT NULL,
+  `course_type` varchar(15) NOT NULL,
+  `avg_rating` double NOT NULL,
+  `status` varchar(15) NOT NULL,
+  `category` varchar(30) NOT NULL,
+  `t_id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`c_id`, `course_name`, `no_of_classes`, `course_type`, `avg_rating`, `status`, `category`, `t_id`) VALUES
+(1, 'Basics to C# Programming', 25, 'Premium', 3.98, 'Complete', 'Programming', 1),
+(2, 'Watercolor Basics', 16, 'Free', 3.66, 'Ongoing', 'Art', 2),
+(3, 'Advanced C# Programming', 25, 'Premium', 4.11, 'Ongoing', 'Programming', 1),
+(4, 'Painting with Gouache', 30, 'Premium', 4.09, 'Complete', 'Art', 2),
+(5, 'Web Development Basics', 20, 'Free', 3.75, 'Complete', 'Programming', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `s_id` int(5) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `gender` varchar(8) NOT NULL,
+  `card no` varchar(20) NOT NULL,
+  `expdate` date NOT NULL,
+  `code` varchar(5) NOT NULL,
+  `usertype` varchar(20) NOT NULL,
+  `propic` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`s_id`, `username`, `email`, `password`, `gender`, `card no`, `expdate`, `code`, `usertype`, `propic`) VALUES
+(1, 'Habiba', 'habiba@aiub.edu', '123', 'Female', '111', '2021-07-31', '000', 'Premium Student', 'GenericPic.jpeg'),
+(3, 'Aelin', 'aelin.agw.@yahoo.com', '111', 'Female', '1234123412341234', '2021-05-05', '111', 'Premium Student', 'GenericPic.jpeg'),
+(4, 'William', 'w&t@yahoo.com', '111', 'Male', '1010202030304040', '2022-09-25', '131', 'Premium Student', 'GenericPic.jpeg'),
+(5, 'Ty', 'tyty@gmail.com', '111', 'Male', '0101020203030404', '2021-07-06', '333', 'Premium Student', 'GenericPic.jpeg'),
+(6, 'Livvy', 'l15@gmail.com', '123', 'Female', '0987098709870', '2021-06-18', '567', 'Basic Student', 'GenericPic.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_list`
+--
+
+CREATE TABLE `student_list` (
+  `l_id` int(5) NOT NULL,
+  `c_id` int(5) NOT NULL,
+  `s_id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student_list`
+--
+
+INSERT INTO `student_list` (`l_id`, `c_id`, `s_id`) VALUES
+(1, 1, 1),
+(2, 1, 3),
+(3, 1, 4),
+(4, 2, 1),
+(5, 2, 3),
+(7, 3, 5),
+(8, 4, 3),
+(9, 4, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teachers`
+--
+
+CREATE TABLE `teachers` (
+  `t_id` int(5) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `gender` varchar(8) NOT NULL,
+  `usertype` varchar(20) NOT NULL,
+  `bank_accno` varchar(20) NOT NULL,
+  `bank_name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `teachers`
+--
+
+INSERT INTO `teachers` (`t_id`, `username`, `email`, `password`, `gender`, `usertype`, `bank_accno`, `bank_name`) VALUES
+(1, 'Cress Darnel', 'crescent@outlook.com', '232', 'Female', 'Teacher', '758455', 'National Bank'),
+(2, 'James Carstairs', 'jc@gmail.com', '777', 'Male', 'Teacher', '245678', 'HSBC'),
+(3, 'Hideo Tanaka', 'tanaka@yahoo.com', '999', 'Male', 'Teacher', '395486', 'HSBC');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`c_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`s_id`);
+
+--
+-- Indexes for table `student_list`
+--
+ALTER TABLE `student_list`
+  ADD PRIMARY KEY (`l_id`);
+
+--
+-- Indexes for table `teachers`
+--
+ALTER TABLE `teachers`
+  ADD PRIMARY KEY (`t_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `c_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `s_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `student_list`
+--
+ALTER TABLE `student_list`
+  MODIFY `l_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `teachers`
+--
+ALTER TABLE `teachers`
+  MODIFY `t_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
